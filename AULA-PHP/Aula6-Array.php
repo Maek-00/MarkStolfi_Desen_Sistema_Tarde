19724971e48447ee3f3135f9488be486811c7146
MarkStolfi <markstolfi27@gmail.com>
2025-04-15 17:01:49 -0300

    Create Aula6-Array.php

---
+++ AULA-PHP/Aula6-Array.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Auala-6 Array</title>
+    </head>
+    <body>
+        <?php
+            $dias = array('Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado');
+            echo $dias[1]."<br>";
+            print_r($dias);
+            echo "<br>";
+            var_dump($dias);
+        ?>
+
+    </body>
+</html> 
